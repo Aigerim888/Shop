403d396a06d34562d9e8a9f291a662824d879519
Aigerim <t_sa06@mail.ru>
2023-03-13 13:34:02 +0600

    Added controller, models

---
+++ Shop/Models/Brands.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Brands
+    {
+        [Key]
+        public int Brand_id{get;set;}
+        public string Name { get; set; }
+
+
+    }
+}
