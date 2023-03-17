660237d8a032645e35c0ece0776ee77084ec2532
Aigerim <t_sa06@mail.ru>
2023-03-17 08:34:51 +0600

    Added index and create files

---
+++ Shop/Controllers/SampleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult GetMessage()
+        {
+            return PartialView("_GetMessage");
+        }
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Я-сообщение";
+            ViewData["Apple"] = 1;
+            ViewBag.Message = "Hello";
+            MyTest obj =new MyTest();
+            obj.Id = 1;
+            obj.Name = "MyName";
+            string myView = "Test";
+            /*var obj = new { id = 1 };*/
+            return View("~/Views/Sample/Index.cshtml",obj);
+            /*return View(myView,obj); 
+            return View(obj);*/
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- Shop/Models/Products.cs
+++ Shop/Models/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shop.Models
 {
@@ -9,10 +10,15 @@ namespace Shop.Models
         public int Product_id { get; set; }
         public string Name { get; set; }
         public string Info { get; set; }
-         public int Category_id {get; set; }
-        public Categories Categories { get; set;}
+        public int Price { get; set; }
+
+        [ForeignKey("Categories")]
+        public int Category_id {get; set; }
+        //public Categories Categories { get; set;}
+
+        [ForeignKey("Brands")]
         public int Brand_id { get; set; }
-        public Brands Brands { get; set;}
+        //public Brands Brands { get; set;}
 
 
 
